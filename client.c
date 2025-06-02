a69f2dda35d1f5f0d0179f63c59fb10f9ea9a4a9
GAUCHE PEREIRA Noah <noah.gauche-pereira@iut-dijon.u-bourgogne.fr>
2025-06-02 16:50:53 +0200

    Update

---
+++ client.c
@@ -0,0 +1,80 @@
+#include "client.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Connects to the server at the given IP address, returns the socket descriptor.
+int connect_to_server(const char *server_ip) {
+    int sock;
+    struct sockaddr_in server_addr;
+    socklen_t addr_len = sizeof(server_addr);
+
+    sock = socket(PF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        exit(-1);
+    }
+    printf("Socket created successfully! (%d)\n", sock);
+
+    memset(&server_addr, 0x00, addr_len);
+    server_addr.sin_family = PF_INET;
+    server_addr.sin_port = htons(SERVER_PORT);
+
+    if (inet_aton(server_ip, &server_addr.sin_addr) == 0) {
+        fprintf(stderr, "Invalid server IP address!\n");
+        close(sock);
+        exit(-2);
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&server_addr, addr_len) == -1) {
+        perror("connect");
+        close(sock);
+        exit(-3);
+    }
+    printf("Connected to server successfully!\n");
+    return sock;
+}
+
+// Sends a message to the server.
+void send_message(int socket, const char *message) {
+    int bytes_sent = write(socket, message, strlen(message));
+    switch (bytes_sent) {
+        case -1:
+            perror("write");
+            close(socket);
+            exit(-4);
+        case 0:
+            fprintf(stderr, "Socket closed by server while sending.\n");
+            close(socket);
+            exit(0);
+        default:
+            printf("Message '%s' sent successfully (%d bytes)\n", message, bytes_sent);
+    }
+}
+
+// Receives a message from the server.
+void receive_message(int socket, char *buffer, int bufsize) {
+    int bytes_received = read(socket, buffer, bufsize);
+    switch (bytes_received) {
+        case -1:
+            perror("read");
+            close(socket);
+            exit(-5);
+        case 0:
+            fprintf(stderr, "Socket closed by server while receiving.\n");
+            close(socket);
+            exit(0);
+        default:
+            buffer[bytes_received] = '\0';
+            printf("Message received from server: %s (%d bytes)\n", buffer, bytes_received);
+    }
+}
+
+// Closes the socket connection.
+void close_connection(int socket) {
+    close(socket);
+    printf("Connection closed.\n");
+}
---
+++ client.h
@@ -0,0 +1,16 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#define SERVER_PORT IPPORT_USERRESERVED
+#define MSG_LEN 256
+
+int connect_to_server(const char *server_ip);
+void send_message(int socket, const char *message);
+void receive_message(int socket, char *buffer, int bufsize);
+void close_connection(int socket);
+
+#endif // CLIENT_H
